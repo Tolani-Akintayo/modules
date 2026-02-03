2e29ca9aa9875358c7eeb56ab7dc02514b75f91d
Tolani Akintayo <akintayotolani100@gmail.com>
2026-02-03 06:48:37 +0100

    created a module for nat-gateway and added output.tf file to the vpc module

---
+++ nat-gateway/main.tf
@@ -0,0 +1,59 @@
+# Elastic IP for NAT gateway
+resource "aws_eip" "eip1" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.environment}-${var.project_name}-eip1"
+  }
+}
+
+# NAT gateway for private subnet internet access
+resource "aws_nat_gateway" "nat_gateway_az1" {
+  allocation_id = aws_eip.eip1.id
+  subnet_id     = var.public_subnet_az2.id
+
+  tags = {
+    Name = "${var.environment}-${var.project_name}-natgw-az1"
+  }
+
+  # Ensure that NAT gateway is created after the internet gateway
+  depends_on = [var.internet_gateway]
+
+}
+
+# Private route table - routes traffic through NAT gateway
+resource "aws_route_table" "private_route_table" {
+  vpc_id = var.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_az1.id
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.project_name}-rtb-private"
+  }
+
+}
+
+# Associate private subnets with private route table  
+
+resource "aws_route_table_association" "private_app_subnet_az1_association" {
+  subnet_id      = var.private_app_subnet_az1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_app_subnet_az2_association" {
+  subnet_id      = var.private_app_subnet_az2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_data_subnet_az1_association" {
+  subnet_id      = var.private_data_subnet_az1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_data_subnet_az2_association" {
+  subnet_id      = var.private_data_subnet_az2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
---
+++ nat-gateway/variables.tf
@@ -0,0 +1,45 @@
+# Variables for the nat gateway
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type = string
+}
+
+variable "project_name" {
+  description = "Project name" 
+  type = string
+}
+
+variable "public_subnet_az2_id" {
+  description = "The ID of public subnet in AZ1"
+  type = string
+}
+
+variable "internet_gateway" {
+  description = "The Internet Gateway"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type = string
+}
+
+variable "private_app_subnet_az1_id" {
+  description = "The ID of private app subnet in AZ1"
+  type = string
+}
+
+variable "private_app_subnet_az2_id" {
+  description = "The ID of private app subnet in AZ2"
+  type = string
+}
+
+variable "private_app_subnet_az2_id" {
+  description = "The ID of private data subnet in AZ1"
+  type = string
+}
+
+variable "private_app_subnet_az2_id" {
+  description = "The ID of private data subnet in AZ2"
+  type = string
+}
