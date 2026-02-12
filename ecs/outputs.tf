7cc42b5f22eb774209e8eb072232012ca7070041
Tolani Akintayo <akintayotolani100@gmail.com>
2026-02-12 21:13:03 +0100

    added values to be exported from ecs for github actions usage

--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -1,4 +1,21 @@
+# ECS 
 output "ecs_service" {
   description = "The ECS service"
   value       = aws_ecs_service.ecs_service
+}
+
+# Added to github actions automation
+output "ecs_service_name" {
+    description = "The name of the ECS service"
+    value = aws_ecs_service.ecs_service.name
+}
+
+output "ecs_cluster_name" {
+    description = "The name of the ECS cluster"
+    value = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_task_definition_name" {
+    description = "The name of the ECS task definition"
+    value = aws_ecs_task_definition.ecs_task_definition.family
 }
