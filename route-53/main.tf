09fc79519e873c867df1fd38273c023ca5cb3e77
Tolani Akintayo <akintayotolani100@gmail.com>
2026-02-18 00:25:57 +0100

    removed ttl from route53 main.tf

--- route-53/main.tf
+++ route-53/main.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "site_domain" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.record_name # e.g. "www" or "app"
   type    = "A"
-  ttl     = 300
+ # ttl     = 300
 
  alias {
    name = var.application_load_balancer_dns_name
